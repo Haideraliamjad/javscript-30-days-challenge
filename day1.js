87441e0911f7280b948b8c74eb9b53b2ec457acb
Haider <haideraliamjad4391@gmail.com>
2024-07-13 05:55:28 -0700

    first day challange complete

---
+++ day1.js
@@ -0,0 +1,23 @@
+var value = 29;
+console.log(value);
+let names = "Haider Ali Amjad";
+console.log(names);
+const isVerifiedEmail = false;
+console.log(isVerifiedEmail);
+let email = "haider.tech.dev@gmail.com";
+const intrests = ["football", "cricket"];
+const favouriteSubjects = {
+  first: "CS",
+  second: "PHY",
+};
+console.log(typeof value);
+console.log(typeof names);
+console.log(typeof email);
+console.log(typeof isVerifiedEmail);
+console.log(typeof intrests);
+console.log(typeof favouriteSubjects);
+
+let someone = "Haider";
+console.log(someone);
+someone = "Hamza";
+console.log(someone);
