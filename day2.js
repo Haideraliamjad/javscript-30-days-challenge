b4b2dac007094741a76c11582d40637d1824ee6f
Haider <haideraliamjad4391@gmail.com>
2024-07-13 23:03:41 -0700

    second day challange complete

---
+++ day2.js
@@ -0,0 +1,30 @@
+// Operators in javascript
+const { log } = console;
+// Arithamtic Operations
+log(5 + 5);
+log(10 - 5);
+log(5 * 5);
+log(10 % 2);
+// Assignment Operations
+let number1 = 10;
+number1 += 4;
+log(number1);
+
+let number2 = 10;
+number2 -= 2;
+log(number2);
+// Comparision Operator
+log(10 > 2);
+log(2 < 10);
+log(10 >= 2);
+log(2 >= 2);
+log(1 == "1");
+log(1 === "1");
+
+// Logical Operators
+log(true && true);
+log(true || true);
+log(!true);
+
+// Ternary Operator
+2 > 0 ? log("positive number") : log("negative number");
